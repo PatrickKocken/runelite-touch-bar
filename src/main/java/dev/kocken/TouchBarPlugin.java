f07ebff41d677b483a218cf63542e9182ed6b751
Patrick Kocken <pah.kocken@gmail.com>
2022-07-13 20:47:06 +0200

    Edit formatting and add simple example in TouchBarPlugin.java

--- src/main/java/dev/kocken/TouchBarPlugin.java
+++ src/main/java/dev/kocken/TouchBarPlugin.java
@@ -1,9 +1,11 @@
 package dev.kocken;
 
 import com.google.inject.Provides;
-import javax.inject.Inject;
+import dev.kocken.nativebridge.JavaTouchBar;
+import dev.kocken.nativebridge.common.Image;
+import dev.kocken.nativebridge.item.TouchBarItem;
+import dev.kocken.nativebridge.item.view.TouchBarButton;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
 import net.runelite.api.events.GameStateChanged;
@@ -12,42 +14,54 @@ import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 
+import javax.inject.Inject;
+import java.awt.*;
+import java.io.IOException;
+
 @Slf4j
 @PluginDescriptor(
-	name = "Touch Bar Plugin"
+        name = "Touch Bar Plugin"
 )
-public class TouchBarPlugin extends Plugin
-{
-	@Inject
-	private Client client;
+public class TouchBarPlugin extends Plugin {
+    @Inject
+    private Client client;
 
-	@Inject
-	private TouchBarPluginConfig config;
+    @Inject
+    private TouchBarPluginConfig config;
 
-	@Override
-	protected void startUp() throws Exception
-	{
-		log.info("Example started!");
-	}
+    @Override
+    protected void startUp() throws Exception {
+        log.info("Example started!");
+    }
 
-	@Override
-	protected void shutDown() throws Exception
-	{
-		log.info("Example stopped!");
-	}
+    @Override
+    protected void shutDown() throws Exception {
+        log.info("Example stopped!");
+    }
 
-	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged)
-	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
-		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
-		}
-	}
+    @Subscribe
+    public void onGameStateChanged(GameStateChanged gameStateChanged) throws IOException {
+        if (gameStateChanged.getGameState() == GameState.LOGGED_IN) {
 
-	@Provides
-	TouchBarPluginConfig provideConfig(ConfigManager configManager)
-	{
-		return configManager.getConfig(TouchBarPluginConfig.class);
-	}
+            Frame[] frames = Frame.getFrames();
+            JavaTouchBar touchBar = new JavaTouchBar();
+
+            Image image = new Image(getClass().getClassLoader().getResourceAsStream("images/inventory.png"));
+
+            TouchBarButton button = new TouchBarButton();
+            button.setImage(image);
+            button.setAction(view -> log.info("Clicked"));
+
+            touchBar.addItem(new TouchBarItem("Button_0", button, true));
+
+            for (Frame f : frames) {
+                touchBar.show(f);
+            }
+        }
+    }
+
+    @Provides
+    TouchBarPluginConfig provideConfig(ConfigManager configManager) {
+        return configManager.getConfig(TouchBarPluginConfig.class);
+    }
 }
